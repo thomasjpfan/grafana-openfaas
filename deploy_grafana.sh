dce16cc233bdf3d2d62f94ef7780afb827ee2f84
Thomas Fan <thomasjpfan@gmail.com>
2018-09-26 15:51:00 -0400

    DOC: Fix docs

--- deploy_grafana.sh
+++ deploy_grafana.sh
@@ -3,7 +3,7 @@ docker service create --name grafana \
 	-p 3000:3000 \
 	-e GF_AUTH_ANONYMOUS_ENABLED=true \
 	-e GF_AUTH_ANONYMOUS_ORG_NAME="Main Org." \
-	-e GR_AUTH_ANONYMOUS_ORG_ROLE=View \
-	-e GR_AUTH_DISABLE_LOGIN_FORM=true \
-	-e GR_AUTH_DISABLE_SIGNOUT_MENU=true \
+	-e GF_AUTH_ANONYMOUS_ORG_ROLE=View \
+	-e GF_AUTH_DISABLE_LOGIN_FORM=true \
+	-e GF_AUTH_DISABLE_SIGNOUT_MENU=true \
 	thomasjpfan/grafana-openfaas
